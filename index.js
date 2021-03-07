0f84331bb54b15660cce18ddae2b1e308e7df70b
Divyanshu Singh <divic309>
2021-03-07 12:10:16 +0530

    Second

---
+++ index.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () = > {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+    //Lets get going with the sound here
+    pads.forEach(pad => {
+        
+    })
+});
